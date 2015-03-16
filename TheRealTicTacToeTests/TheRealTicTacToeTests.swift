9d40147b7db87e43970e56034cee1b0ad92168c2
Antonio Rodrigues <aaadonai@gmail.com>
2015-03-15 23:34:59 -0700

    Added unit test

--- TheRealTicTacToeTests/TheRealTicTacToeTests.swift
+++ TheRealTicTacToeTests/TheRealTicTacToeTests.swift
@@ -11,9 +11,12 @@ import XCTest
 
 class TheRealTicTacToeTests: XCTestCase {
     
+    var engine:PerfectPlayer!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        engine = PerfectPlayer.init(state: BoardState())
     }
     
     override func tearDown() {
@@ -31,6 +34,56 @@ class TheRealTicTacToeTests: XCTestCase {
         self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testEngine() {
+        
+        //This is a perfect game, two perfect players playing against each other. 
+        //It can only result in draw
+        var m1: AnyObject? = engine.moveFromSearch(9)
+        
+        engine.performMove(m1!)
+        
+        var m2: AnyObject? = engine.moveFromSearch(8)
+        
+        engine.performMove(m2!)
+
+        var m3: AnyObject? = engine.moveFromSearch(7)
+        
+        engine.performMove(m3!)
+
+        var m4: AnyObject? = engine.moveFromSearch(6)
+        
+        engine.performMove(m4!)
+
+        var m5: AnyObject? = engine.moveFromSearch(5)
+        
+        engine.performMove(m5!)
+
+        var m6: AnyObject? = engine.moveFromSearch(4)
+        
+        engine.performMove(m6!)
+
+        var m7: AnyObject? = engine.moveFromSearch(3)
+        
+        engine.performMove(m7!)
+        
+        var m8: AnyObject? = engine.moveFromSearch(2)
+        
+        engine.performMove(m8!)
+        
+        XCTAssertFalse(engine.isGameOver(), "false")
+
+        var m9: AnyObject? = engine.moveFromSearch(1)
+        
+        engine.performMove(m9!)
+
+        
+        XCTAssertTrue(engine.isGameOver(), "true")
+        
+        var state = engine.currentState()
+        
+        XCTAssertTrue(state.isDraw(), "true")
     }
     
 }
